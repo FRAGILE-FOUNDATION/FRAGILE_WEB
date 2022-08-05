00907cc9921284af2651565094f114f050880d63
Nandy Bâ <nandyba@yahoo.fr>
2022-08-06 00:14:49 +0200

    Show nft template

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,11 @@ import './App.css';
 import { useMoralis } from "react-moralis";
 import 'antd/dist/antd.css';
 import 'antd/dist/antd.js';
+import { Typography, Row, Col } from 'antd';
+import './Home.css';
+
+const { Text } = Typography;
+
 
 const Home = () => {
 
@@ -38,6 +43,7 @@ const Home = () => {
     }
 
     let buttons = (<button onClick={logOut} disabled={isAuthenticating}>Logout</button>);
+
     if(!user){
       buttons = (<button onClick={login}>Moralis Metamask Login</button>);
     }
@@ -46,6 +52,27 @@ const Home = () => {
     <div>
       <h1>Moralis Hello World!</h1>
       {buttons}
+      {(user) ? user.get("ethAddress") : ''}
+      <Row style={{marginBottom:'20px'}}>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3001/"></img>
+        </Col>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3002/"></img>
+        </Col>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3003/"></img>
+        </Col>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3004/"></img>
+        </Col>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3005/"></img>
+        </Col>
+        <Col span={6} offset={1}>
+          <img className="nft_show" src="https://picsum.photos/2000/3006/"></img>
+        </Col>
+      </Row>
     </div>
   );
 }
