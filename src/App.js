e5fef2304d6e71c03f67fbee79f6d2e432b4ea30
Nandy Bâ <nandyba@yahoo.fr>
2022-08-06 19:30:15 +0200

    Move antd importation to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./index.css";
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.js';
 import { MoralisProvider } from "react-moralis";
-import Home from "./Home";
-import ShowCollection from './ShowCollection';
+import Drop from "./page/Drop";
 
 const App = () => {
 
   return (
     <MoralisProvider serverUrl="https://gnqq6qelfdee.usemoralis.com:2053/server" appId="OeFnFXYeJsvYKcuqAHko2a9fPNmn9sqmQvLnYYug">
-      {/*<Home/>*/}
-      <ShowCollection/>
+      <Drop/>
     </MoralisProvider>
   );
 
