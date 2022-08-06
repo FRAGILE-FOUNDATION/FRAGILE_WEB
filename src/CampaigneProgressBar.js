4cf18c0cb7a93331948e0b5a71ad355508451be8
Nandy Bâ <nandyba@yahoo.fr>
2022-08-06 15:15:15 +0200

    ProgressBar

--- src/CampaigneProgressBar.js
+++ src/CampaigneProgressBar.js
@@ -1,10 +1,10 @@
 import { Progress } from 'antd';
 import './CampaigneProgressBar.css';
 
-const CampaigneProgressBar = ({amount, goalAmount}) => {
+const CampaigneProgressBar = ({amount, goalAmount, strokeColor="#5CB0FF", trailColor="#E2EAF2"}) => {
     return (
         <>
-            <Progress percent={30} showInfo={false} strokeColor="#5CB0FF"  trailColor="#E2EAF2" className='campaign-progressBar'/>
+            <Progress percent={30} showInfo={false} strokeColor={strokeColor}  trailColor={trailColor} className='campaign-progressBar'/>
             <div className='campaign-current-donation'>{Math.round(amount)}ETH</div>
             <div className='campaign-goal'>Donated of {goalAmount}ETH Gloal</div>
         </>
