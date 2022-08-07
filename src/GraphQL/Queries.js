fdd473458dcb17d14c2fbbe064577591969515fe
Nandy Bâ <nandyba@yahoo.fr>
2022-08-07 20:00:00 +0200

    Load Editions

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -1,15 +1,15 @@
 import { gql } from "@apollo/client";
 
-export const LOAD_NFTs = gql`
+const LOAD_Paladium_NFTs= gql`
     query MyQuery {
-        collections(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x5b940c58273964bdbb7d1f7389122d3bfab5737d"}) {
+        collections(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x099cd1559076b24505c653b7da155d0cc17bb8c9"}) {
             nodes {
                 name
                 description
                 address
             }
         }
-        tokens(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x5b940c58273964bdbb7d1f7389122d3bfab5737d"}) {
+        tokens(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x099cd1559076b24505c653b7da155d0cc17bb8c9"}) {
             nodes {
                 token {
                     tokenId
@@ -29,3 +29,65 @@ export const LOAD_NFTs = gql`
         }
     }  
 `;
+
+const LOAD_Rhodium_NFTs= gql`
+    query MyQuery {
+        collections(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x44177241427655984ab86ae605b5b9ec2106addc"}) {
+            nodes {
+                name
+                description
+                address
+            }
+        }
+        tokens(networks: {chain: RINKEBY}, where: {collectionAddresses: "0x44177241427655984ab86ae605b5b9ec2106addc"}) {
+            nodes {
+                token {
+                    tokenId
+                    name
+                    owner
+                    attributes {
+                        traitType
+                        value
+                    }
+                    image {
+                        url
+                        mimeType
+                    }
+                }
+                
+            }
+        }
+    }  
+`;
+
+const LOAD_Iridium_NFTs= gql`
+    query MyQuery {
+        collections(networks: {chain: RINKEBY}, where: {collectionAddresses: "0xadcc0d343c818151adf1502a535faed7ae1cdd49"}) {
+            nodes {
+                name
+                description
+                address
+            }
+        }
+        tokens(networks: {chain: RINKEBY}, where: {collectionAddresses: "0xadcc0d343c818151adf1502a535faed7ae1cdd49"}) {
+            nodes {
+                token {
+                    tokenId
+                    name
+                    owner
+                    attributes {
+                        traitType
+                        value
+                    }
+                    image {
+                        url
+                        mimeType
+                    }
+                }
+                
+            }
+        }
+    }  
+`;
+
+export {LOAD_Paladium_NFTs, LOAD_Rhodium_NFTs, LOAD_Iridium_NFTs};
--- src/QueryPage.js
+++ src/QueryPage.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { LOAD_NFTs } from "./GraphQL/Queries";
+import {LOAD_Paladium_NFTs, LOAD_Rhodium_NFTs, LOAD_Iridium_NFTs} from "./GraphQL/Queries";
 
-function GetNFTs() {
-  const { error, data } = useQuery(LOAD_NFTs);
+const NFTCollection = [LOAD_Paladium_NFTs, LOAD_Rhodium_NFTs, LOAD_Iridium_NFTs]
+
+function GetSpecificCollection(collection_loader) {
+  const { error, data } = useQuery(LOAD_Paladium_NFTs);
   useEffect(() => {
-    if (data) {
-        console.log(data);
-    }
-    else if(error){
+    if(error){
         console.error(data);
     }
   }, [data, error]);
 
   return (
-    <div>
-    </div>
+    data
   );
+}
+
+function GetNFTs() {
+  console.log(NFTCollection.map(x => GetSpecificCollection(x)));
+  return GetNFTs;
 }
 
 export default GetNFTs;
