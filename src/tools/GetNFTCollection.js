0aca9473f9bcffcbb04499bdb833848835a2e399
Nandy Bâ <nandyba@yahoo.fr>
2022-08-07 11:59:35 +0200

    Clean code

--- src/tools/GetNFTCollection.js
+++ src/tools/GetNFTCollection.js
@@ -59,11 +59,4 @@ const useCollectionNFTS = ({collectionAddresses}) =>{
   
 }
 
-function jsonConcat(o1, o2) {
-    for (var key in o2) {
-        o1[key] = o2[key];
-    }
-    return o1;
-}
-
 export {useCollectionNFTS, getCollectionData};
